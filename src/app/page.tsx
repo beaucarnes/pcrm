c04f377b05ab3bb8b9bbe7e53d1888819424666d
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2025-02-09 17:46:11 -0500

    add scroll to top button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from 'next/navigation'
+'use client'
 
-export default function Home() {
-  redirect('/contacts')
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function HomePage() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/contacts')
+  }, [router])
+
+  return null
 }
